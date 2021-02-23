80212d28d42738933142fe6f0c20c931c552246a
sancho <liviasancho@gmail.com>
2021-02-23 19:30:30 -0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="LiviaSancho",
+    author_email="liviasancho@gmail.com",
+    packages=["mysci"],
+    install_requires=[])
